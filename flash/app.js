f9c7c6cf0543c8047073075a5f38a527236705b2
Guillaume <gumgl@users.noreply.github.com>
2015-03-31 20:50:53 -0400

    /flash: Start development

---
+++ flash/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var routes = require('./routes/index');
+var fs = require('fs');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(multer( {
+    dest: 'uploads/',
+    rename: function(fieldname, filename) { return filename }
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'files')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+module.exports = app; 
